5ec0a4b654b7d098bbff1a06b3d2e34726e41864
pablo-rank <50866155+pablo-rank@users.noreply.github.com>
2021-04-26 16:24:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '',
-       'index.html',
-       'index.js',
-       'style.css',
-       'images/fox1.jpg',
-       'images/fox2.jpg',
-       'images/fox3.jpg',
-       'images/fox4.jpg',
-       'images/fox5.jpg'
+       './',
+       './index.html',
+       './index.js',
+       './style.css',
+       './images/fox1.jpg',
+       './images/fox2.jpg',
+       './images/fox3.jpg',
+       './images/fox4.jpg',
+       './images/fox5.jpg'
      ]);
    })
  );
